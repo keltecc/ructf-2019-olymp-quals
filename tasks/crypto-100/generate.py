31dc805396dd58fd7004788dedbd0e5a7df99df5
Vladimir <keltecc@ya.ru>
2019-02-14 13:22:52 +0500

    update crypto-100

--- tasks/crypto-100/generate.py
+++ tasks/crypto-100/generate.py
@@ -2,37 +2,33 @@
 
 import sys
 
-from random import randrange
-
-
-def to_number(message):
-    h_message = bytes(message, 'utf-8').hex()
-    return int(h_message, 16)
+from random import randint
 
 
 def make_modulus(bits):
     bits >>= 1
-    return randrange(2**bits) * randrange(2**bits)
+    p = randint(2**(bits-1), 2**bits)
+    q = randint(2**(bits-1), 2**bits)
+    return p * q
 
 
 def encrypt(message, key):
     exponent, modulus = key
-    plaintext = to_number(message)
+    plaintext = int.from_bytes(message.encode('utf-8'), 'big')
     assert plaintext ** exponent > modulus
     return pow(plaintext, exponent, modulus)
 
 
 def main():
-    exponent, bits = 3, 1280
+    exponent, bits = 3, 900
 
     if len(sys.argv) < 2:
         print('usage: {} <your_flag>'.format(sys.argv[0]))
         sys.exit(-1)
 
-    plaintext = '[*] Your secret flag is ' + sys.argv[1]
+    plaintext = sys.argv[1]
     modulus = make_modulus(bits)
-    key = exponent, modulus
-    ciphertext = encrypt(plaintext, key)
+    ciphertext = encrypt(plaintext, (exponent, modulus))
 
     known_part = '0x' + (plaintext.encode()[:-32] + b'\x00' * 32).hex()
     print('exponent = ' + str(exponent))
