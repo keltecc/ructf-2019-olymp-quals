5c302424b1578151531c5d8179c266963b743a7b
Vladimir <keltecc@ya.ru>
2019-03-06 02:50:45 +0500

    add web-100

--- tasks/web-100/server.py
+++ tasks/web-100/server.py
@@ -1,50 +1,70 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, session
-from flask import make_response, abort, redirect, url_for
-from flask import render_template, send_from_directory
+import re
 
-from os import environ
-from hashlib import sha256
+from flask import Flask, request, session, redirect, url_for, render_template, make_response
+from utils import check_username, check_filename, check_content, enumerate_files, read_file, write_file
 
 
-app = Flask(__name__, static_folder='./static/')
+app = Flask(__name__, static_url_path='', static_folder='./static/')
 app.secret_key = 'p13453_d0_n07_h4ck_17'
 
 
-def check_username(username):
-    return username != 'admin'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return redirect(url_for('index'))
+    username = request.form.get('username', None)
+    if request.method == 'GET' or username is None:
+        return render_template('login.html')
+    if not check_username(username):
+        return render_template('login.html', error='Logging in with specified username is highly prohibited!')
+    session['username'] = username
+    return redirect(url_for('index'))
 
 
-def calculate_hash(username):
-    salt = ''.join(map(''.join, environ.items()))
-    data = salt + username + salt
-    return sha256(data.encode()).hexdigest()
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if 'username' in session:
+        del session['username']
+    return redirect(url_for('index'))
+
+
+@app.route('/read', methods=['GET'])
+def read():
+    username = session.get('username', None)
+    filename = request.args.get('filename', None)
+    if username is None:
+        return make_response('error: you must be logged in!')
+    if filename is None:
+        return make_response('error: specify a filename!')
+    return make_response(read_file(username, filename))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    action = request.form.get('action', None)
     username = session.get('username', None)
-    if username is None and action != 'login':
-        return render_template('login.html')
-    if request.method == 'GET' or 'action' is None:
-        return render_template('index.html', username=username)
-    if action == 'logout':
-        del session['username']
-        return render_template('login.html')
-    if action == 'login':
-        if username is not None:
-            return render_template('index.html', username=username, error='You have already logged in!')
-        username = request.form.get('username', None)
-        if username is None:
-            return render_template('login.html', error='You should specify a username!')
-        if not check_username(username):
-            return render_template('login.html', error='Logging in with specified username is highly prohibited!')
-        session['username'] = username
-        return render_template('index.html', username=username)
-    return render_template('index.html', username=username, error='Wrong action! Do you attempt to hack?')
+    if username is None:
+        return redirect(url_for('login'))
+    files = enumerate_files(username)
+    filename = request.form.get('filename', None)
+    content = request.form.get('content', None)
+    if request.method == 'GET' or filename is None or content is None:
+        return render_template('index.html', files=files)
+    if not check_filename(filename):
+        return render_template('index.html', files=files, error='Invalid filename!')
+    if not check_content(content):
+        return render_template('index.html', files=files, error='Content is too long or invalid!')
+    write_file(username, filename, content)
+    return render_template('index.html', files=enumerate_files(username))
+
+
+def _put_flag():
+    filename = 'flag.txt'
+    with open(filename, 'r') as file:
+        write_file('admin', filename, file.read())
 
 
 if __name__ == '__main__':
-    app.run(port=8888, threaded=True)
+    _put_flag()
+    app.run('0.0.0.0', port=8888, threaded=True)
---
+++ tasks/web-100/utils.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import re
+
+from os import environ, makedirs
+from glob import glob
+from string import printable
+from hashlib import sha256
+
+from os.path import curdir, isfile, isdir, dirname, realpath, join, basename
+
+
+def check_username(username):
+    return username != 'admin'
+
+
+def check_filename(filename):
+    pattern = '^\w{1,20}(\.\w{1,4})?$'
+    match = re.match(pattern, filename)
+    return bool(match)
+
+
+def check_content(content):
+    return len(content) < 100 and all(symbol in printable for symbol in content)
+
+
+def enumerate_files(username):
+    return list(map(basename, glob(_build_path(username, '*'))))
+
+
+def read_file(username, filename):
+    path = _build_path(username, filename)
+    if not _check_path(path):
+        return 'error: hacking attempt detected!'
+    if not isfile(path):
+        return 'error: file does not exist!'
+    with open(path, 'rb') as file:
+        return file.read()
+
+
+def write_file(username, filename, content):
+    path = _build_path(username, filename)
+    _create_dir_if_not_exist(dirname(path))
+    with open(path, 'wb') as file:
+        file.write(content.encode())
+
+
+def _create_dir_if_not_exist(path):
+    if not isdir(path):
+        makedirs(path)
+
+
+def _check_path(path):
+    pathdir = dirname(realpath(path))
+    current = realpath(curdir)
+    return pathdir.startswith(current) and pathdir != current
+
+
+def _build_path(username, filename):
+    return join(curdir, 'data', _calculate_hash(username), filename)
+
+
+def _calculate_hash(username):
+    salt = ''.join(map(''.join, environ.items()))
+    data = salt + username + salt
+    return sha256(data.encode()).hexdigest()
